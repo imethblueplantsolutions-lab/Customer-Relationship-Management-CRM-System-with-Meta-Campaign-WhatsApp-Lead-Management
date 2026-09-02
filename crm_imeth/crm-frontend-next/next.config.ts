861e733dae5060502859017c5a22cf48e701a53c
Imeth Dewmina Rathnayaka <imethblueplantsolutions@gmail.com>
2026-09-02 16:15:21 +0530

    ngrok configured

--- crm_imeth/crm-frontend-next/next.config.ts
+++ crm_imeth/crm-frontend-next/next.config.ts
@@ -1,8 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ["excavate-surround-punctured.ngrok-free.dev"],
   turbopack: {
     root: __dirname,
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:4000/api/:path*",
+      },
+    ];
   },
 };
 
